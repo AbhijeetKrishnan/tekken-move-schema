34c878c66e86021094f027412e9e22889407655b
Abhijeet Krishnan <abhijeet.krishnan@gmail.com>
2023-11-03 18:39:30 -0400

    Add hideable notes area

--- ui-experiment/script.js
+++ ui-experiment/script.js
@@ -1,6 +1,20 @@
 // Specify the path to your CSV file
 const csvFilePath = 'bryan-t8.csv';
 
+// Function to create an expandable area for notes
+function createNotesArea(notes) {
+    const notesContainer = document.createElement('div');
+    notesContainer.className = 'notes-container';
+    notesContainer.style.display = 'none'; // Initially hide the notes
+
+    const notesContent = document.createElement('p');
+    notesContent.textContent = notes;
+
+    notesContainer.appendChild(notesContent);
+
+    return notesContainer;
+}
+
 // Function to load the CSV file
 function loadCsvFile() {
     fetch(csvFilePath)
@@ -22,6 +36,7 @@ function loadCsvFile() {
             propsRow.forEach((prop) => {
                 const th = document.createElement('th');
                 th.textContent = prop;
+                th.classList.add(prop);
                 headerRow.appendChild(th);
             });
 
@@ -34,8 +49,28 @@ function loadCsvFile() {
                     for (let j = 0; j < propsRow.length; j++) {
                         // Add cells to the row
                         const cell = newRow.insertCell(j);
-                        cell.textContent = row[propsRow[j]];
-                        cell.classList.add(propsRow[j]);
+                        if (propsRow[j] === 'notes') {
+                            const notesButton = document.createElement('div');
+                            notesButton.className = 'notes-button';
+                            cell.appendChild(notesButton);
+                            cell.appendChild(createNotesArea(row[propsRow[j]]));
+
+                            // Add an event listener to toggle the visibility of notes and change the arrow style
+                            notesButton.addEventListener('click', function () {
+                                const notesContainer = cell.querySelector('.notes-container');
+                                if (notesContainer.style.display === 'none') {
+                                    notesContainer.style.display = 'block';
+                                    notesButton.classList.add('expanded');
+                                } else {
+                                    notesContainer.style.display = 'none';
+                                    notesButton.classList.remove('expanded');
+                                }
+                            });
+                            cell.classList.add(propsRow[j]);
+                        } else {
+                            cell.textContent = row[propsRow[j]];
+                            cell.classList.add(propsRow[j]);
+                        }
                     }
                 }
             }
